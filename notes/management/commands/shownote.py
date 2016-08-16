e74146fbd1dee25bacd62a7a568c158392b8329f
Maxime Laudrin <maximedelaudrin@gmail.com>
2016-08-16 18:35:53 +0200

    Added basic note creation

---
+++ notes/management/commands/shownote.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import tempfile
+import subprocess
+from django.core.management.base import BaseCommand, CommandError
+from notes.models import Note
+
+
+class Command(BaseCommand):
+    help = 'Show the details of a note'
+
+    def add_arguments(self, parser):
+        parser.add_argument('note_id', type=int,
+                            help='the id of the note')
+
+    def handle(self, *args, **options):
+        note_id = options['note_id']
+        try:
+            note = Note.objects.get(pk=note_id)
+        except Note.DoesNotExist:
+            raise CommandError('Note "%s" does not exist' % note_id)
+
+        s = "id:  %i\ncreated at:  %s\ncontent:\n%s\n" % (note.id, note.created, note.text)
+
+        self.stdout.write(s)
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        l = len(self.text)
+        return self.text[:50] + ('...' if l > 50 else '')
