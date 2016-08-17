1a0e977b0134a6278763f6d34ce5ec3708d6d0d4
Maxime Laudrin <maximedelaudrin@gmail.com>
2016-08-17 22:09:59 +0200

    Added Project model and 'note new' command

--- notes/admin.py
+++ notes/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Project
 
 
 class NoteAdmin(admin.ModelAdmin):
-    fields = ('user', 'created', 'text')
     readonly_fields = ('created',)
+    fields = ('user', 'project', 'references', 'text')
 
 admin.site.register(Note, NoteAdmin)
+admin.site.register(Project)
--- notes/models.py
+++ notes/models.py
@@ -2,10 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Note(models.Model):
-    text = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,
+                               related_name='children', blank=True, null=True)
+
+    name = models.CharField(max_length=32, blank=False)
+    description = models.CharField(max_length=80, blank=True)
+
+    def full_name(self):
+        if self.parent:
+            return self.parent.__str__() + '.' + self.name
+        else:
+            return self.name
+
+    def __str__(self):
+        return self.full_name()
+
+    class Meta:
+        unique_together = ('user', 'parent', 'name')
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='notes', blank=True, null=True)
+
+    text = models.TextField(blank=False)
+    created = models.DateTimeField(auto_now_add=True)
     references = models.ManyToManyField('self', db_table='references', symmetrical=False,
                                         related_name='referencers')
     rank = models.IntegerField(default=1)
